5416971c08b06fc43da7982c828ad3fd79f3ed25
qwk <80052118+qwkdev@users.noreply.github.com>
2024-09-24 23:44:03 +0100

    Create example.user.js

---
+++ mods/example.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Example Script
+// @namespace   xmods
+// @match       *://example.org/*
+// @grant       none
+// @version     1.0
+// @author      qwk
+// ==/UserScript==
+
